9229c16535b1056cfd7de500623b78d0c0f4c588
khandaremanisha9 <khandaremanisha9@gmail.com>
2025-02-19 18:30:45 +0530

    Create B.java

---
+++ B.java
@@ -0,0 +1,8 @@
+// Task 2: Create class B which extends class A
+public class B extends A {
+    // Task 4: Override the method `meth` in class B
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
